394265c6900ea67a3d06d19a859125cfb00f7514
bennyguo <bennyguo@163.com>
2017-04-20 20:17:08 +0800

    Change app names.

--- InfoSystem/settings.py
+++ InfoSystem/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'frontend',
-    'backend',
+    'dashboard',
+    'participation',
 ]
 
 MIDDLEWARE = [
