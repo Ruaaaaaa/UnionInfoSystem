aabbba9093d02ddc79273f923cde4f5f673212ac
bennyguo <bennyguo@163.com>
2017-04-25 22:13:39 +0800

    Frontend file structure.

--- participation/views.py
+++ participation/views.py
@@ -16,9 +16,34 @@ from base import sessions
 
 @require_http_methods(['GET', 'POST'])
 def login(request):
-	return render(request, 'participation/login.html', {'title': '用户登陆'})
+	return render(request, 'participation/login.html', {})
 
 
 @require_http_methods(['GET', 'POST'])
 def register(request):
-	return render(request, 'participation/register.html', {'title': '用户注册'})
+	return render(request, 'participation/register.html', {})
+
+
+@require_http_methods(['GET'])
+#@login_required
+def activity(request, aaid):
+	return render(request, 'participation/activity.html', {'aaid': aaid})
+
+
+@require_http_methods(['GET', 'POST'])
+#@login_required
+def checkin(request, aaid):
+	return render(request, 'participation/checkin.html', {'aaid': aaid})
+
+
+@require_http_methods(['GET'])
+#@login_required
+def checkinSuccess(request):
+	return render(request, 'participation/checkin_success.html', {})
+
+
+@require_http_methods(['GET'])
+#@login_required
+def checkinFail(request):
+	return render(request, 'participation/checkin_fail.html', {})
+
