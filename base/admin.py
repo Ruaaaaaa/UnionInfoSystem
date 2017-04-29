ce4129e206f316612c69edb8b8072620aa7cb602
Colin <l-mh@qq.com>
2017-04-29 16:00:48 +0800

    add django admin tool

--- base/admin.py
+++ base/admin.py
@@ -2,5 +2,14 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from base.models import *
 
 # Register your models here.
+admin.site.register(Department)
+admin.site.register(Subunion)
+admin.site.register(User)
+admin.site.register(Activity)
+admin.site.register(Broadcast)
+admin.site.register(Record)
+admin.site.register(Message)
+
